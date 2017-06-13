279e6105a530a4c1d4b0260394255e99310314c4
riquet28 <eric.merlin3@gmail.com>
2017-06-13 02:36:04 +0200

    Modification des formulaires

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
+      devise_parameter_sanitizer.for(:sign_up) << :fullname
+      devise_parameter_sanitizer.for(:account_update) << :fullname
     end
 
 end
